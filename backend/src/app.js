51c368925460cff70ad866c0d20c5e6af7533a2b
Davidson <dehpacifico@gmail.com>
2023-10-27 22:34:23 -0300

    mudando-rotas-para-arquivo-routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,8 @@
 const express = require('express')
+const router = require('./router')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('BACK EEND')
-})
+app.use(router)
 
 module.exports = app;
