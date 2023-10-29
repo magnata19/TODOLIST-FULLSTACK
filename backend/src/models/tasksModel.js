36a01958c9e83039b027791646434f0e9356770c
Davidson <dehpacifico@gmail.com>
2023-10-29 11:22:34 -0300

    feito conexao com db e criando task

--- backend/src/models/tasksModel.js
+++ backend/src/models/tasksModel.js
@@ -2,10 +2,18 @@ const connection = require("./connection");
 
 //esse método me retorna todas tasks do banco de dados
 const getAll = async () => {
-  const tasks = await connection.execute("SELECT * FROM tasks");
+  const [tasks] = await connection.execute("SELECT * FROM tasks");
   return tasks;
 };
 
+const createTask = async (task) => {
+  const { title } = task;
+  const query = 'INSERT INTO tasks(title, status, created_at) VALUES(?, ?, ?)';
+  
+  const createdTask = await connection.execute(query, [title, 'pendente', 'daser'])
+}
+
 module.exports = {
   getAll,
+  createTask
 };
